b1bb8b31838570deca991c0b3ed917e539a6dc89
JuanK854 <juanc854ypk@gmail.com>
2026-03-11 07:57:07 -0600

    feat: TimerSection.jsx componente visual para el timer

---
+++ src/components/organisms/TimerSection.jsx
@@ -0,0 +1,29 @@
+import TimerDisplay from "../atoms/TimerDisplay";
+import TimerControls from "../molecules/TimerControls";
+
+export default function TimerSection({ time, isRunning, onStart, onStop, onReset, mode, onModeChange }) {
+    return(
+        <div className="flex flex-col items-center justify-center gap-8 h-full"> 
+            <div className="flex gap-4">
+                <button
+                    onClick={()=>onModeChange("trabajo")}
+                    className={`px-6 py-2 rpunded-x1 font-semibold ${mode ==="trabajo" ? "bg-amber-50 text-black" : "bg-zinc-800 text-amber-50"}`}> 
+                        {/*el if cambia el color segun el modo*/}
+                        Trabajo
+                </button>
+                <button
+                    onClick={()=>onModeChange("descanso")}
+                    className={`px-6 py-2 rounded-xl font-semibold ${mode==="descando" ? "bg-amber-50 text-black" : "bg-zinc-800 text-amber-50"}`}>
+                        Descanso
+                </button>
+            </div>
+            <TimerDisplay time={time} />
+            <TimerControls
+                isRunning={isTunning}
+                onStart={onStart}
+                onStop={onStop}
+                onReset={onReset}
+            />
+        </div>
+    )
+}
