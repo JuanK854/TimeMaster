00af232382d659d749e28fd10a603836e18b7306
JuanK854 <juanc854ypk@gmail.com>
2026-03-12 15:34:46 -0600

    feat: useTimer.js y TimerSection.jsx
    agrega la logica que useTimer.js uqe controla el estado del pomodoro asi como de los botones y TimerSection.jsx la parte visual del pomodoro junto a los props de su funcionamiento

--- src/components/organisms/TimerSection.jsx
+++ src/components/organisms/TimerSection.jsx
@@ -7,13 +7,13 @@ export default function TimerSection({ time, isRunning, onStart, onStop, onReset
             <div className="flex gap-4">
                 <button
                     onClick={()=>onModeChange("trabajo")}
-                    className={`px-6 py-2 rpunded-x1 font-semibold ${mode ==="trabajo" ? "bg-amber-50 text-black" : "bg-zinc-800 text-amber-50"}`}> 
+                    className={`px-6 py-2 rounded-xl font-semibold ${mode ==="trabajo" ? "bg-amber-50 text-black" : "bg-zinc-800 text-amber-50"}`}> {/*Cambio de sintaxis en el tailwind de rpounded to rounded */} 
                         {/*cambia el color del boton segun el modo(string)*/}
                         Trabajo
                 </button>
                 <button
                     onClick={()=>onModeChange("descanso")}
-                    className={`px-6 py-2 rounded-xl font-semibold ${mode==="descando" ? "bg-amber-50 text-black" : "bg-zinc-800 text-amber-50"}`}>
+                    className={`px-6 py-2 rounded-xl font-semibold ${mode==="descanso" ? "bg-amber-50 text-black" : "bg-zinc-800 text-amber-50"}`}>
                         Descanso
                 </button>
             </div>
