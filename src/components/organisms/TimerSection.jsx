0ab2c8b9453a566bde803f8d7fb28d87257e2fdb
JuanK854 <juanc854ypk@gmail.com>
2026-03-12 16:04:27 -0600

    fix: cambio el hover para que muestre el puntero al momento de estar arriba del boton

--- src/components/organisms/TimerSection.jsx
+++ src/components/organisms/TimerSection.jsx
@@ -7,13 +7,13 @@ export default function TimerSection({ time, isRunning, onStart, onStop, onReset
             <div className="flex gap-4">
                 <button
                     onClick={()=>onModeChange("trabajo")}
-                    className={`px-6 py-2 rounded-xl font-semibold ${mode ==="trabajo" ? "bg-amber-50 text-black" : "bg-zinc-800 text-amber-50"}`}> {/*Cambio de sintaxis en el tailwind de rpounded to rounded */} 
+                    className={`px-6 py-2 rounded-xl font-semibold ${mode ==="trabajo" ? "bg-amber-50 text-black" : "bg-zinc-800 text-amber-50"} hover:cursor-pointer`}> {/*Cambio de sintaxis en el tailwind de rpounded to rounded */} 
                         {/*cambia el color del boton segun el modo(string)*/}
                         Trabajo
                 </button>
                 <button
                     onClick={()=>onModeChange("descanso")}
-                    className={`px-6 py-2 rounded-xl font-semibold ${mode==="descanso" ? "bg-amber-50 text-black" : "bg-zinc-800 text-amber-50"}`}>
+                    className={`px-6 py-2 rounded-xl font-semibold ${mode==="descanso" ? "bg-amber-50 text-black" : "bg-zinc-800 text-amber-50"} hover:cursor-pointer`}>
                         Descanso
                 </button>
             </div>
@@ -27,3 +27,5 @@ export default function TimerSection({ time, isRunning, onStart, onStop, onReset
         </div>
     )
 }
+
+/*Fix del hover para puntero de los botones */
