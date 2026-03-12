71856aa263791f24481fe18e5363463d251ba3e9
JuanK854 <juanc854ypk@gmail.com>
2026-03-12 03:05:04 -0600

    fix: añade TimerPage como import a App.jsx junto a su Route para que se muestre en la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import MainLayout from './components/templates/MainLayout';
+import TimerPage from './pages/TimerPage';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<MainLayout/>}>
-
+                    <Route index element={<TimerPage/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
