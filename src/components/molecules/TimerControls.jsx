d45e468be14c3337794b97d72ae34f4b382e15bf
JuanK854 <juanc854ypk@gmail.com>
2026-03-12 18:13:14 -0600

    fix: botones reactivos en TimerControls.jsx (intento)

--- src/components/molecules/TimerControls.jsx
+++ src/components/molecules/TimerControls.jsx
@@ -4,10 +4,10 @@ import Button from "../atoms/Button"
 export default function TimerControls({ onStart, onStop, onReset, isRunning }) {
     return(
         <div className="flex gap-4">
-            {!isRunning ?(<Button onClick={onStart} variant="primary" className="px-10 py-4 text-xl" > Iniciar </Button>) //En pausa pasa a iniciar
-            : (<Button onClick={onStop} variant="secondary" className="px-10 py-4 text-xl" > Detener </Button>)}  /*sino detener*/
+            {!isRunning ?(<Button onClick={onStart} variant="primary" className="px-6 py-2 text-base md:px-10 md:py-4 md:text-xl" > Iniciar </Button>) //En pausa pasa a iniciar
+            : (<Button onClick={onStop} variant="secondary" className="px-6 py-2 text-base md:px-10 md:py-4 md:text-xl" > Detener </Button>)}
 
-            <Button onClick={onReset} variant="secondary" className="px-10 py-4 text-xl" > Reiniciar </Button>    {/*Independiente siempre visible*/}
+            <Button onClick={onReset} variant="secondary" className="px-6 py-2 text-base md:px-10 md:py-4 md:text-xl" > Reiniciar </Button>    {/*Independiente siempre visible*/}
         </div>
     )
 }
