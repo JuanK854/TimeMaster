bd2005beaf202e845dedab7c9a56fb41f7b593d1
JuanK854 <juanc854ypk@gmail.com>
2026-03-13 17:00:57 -0600

    fix: agrego el import y Route de HabitTrackerPage.js a App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import MainLayout from "./components/templates/MainLayout"
 import TimerPage from "./pages/TimerPage"
 import CalendarPage from "./pages/CalendarPage"
-
+import HabitTrackerPage from "./pages/HabitTrackerPage"
 
 export default function App() {
     return (
@@ -11,7 +11,7 @@ export default function App() {
                 <Route path="/" element={<MainLayout />}>
                     <Route index element={<TimerPage />} />
                     <Route path="calendario" element={<CalendarPage />} />
-
+                    <Route path="habitos" element={<HabitTrackerPage />} />
                 </Route>
             </Routes>
         </BrowserRouter>
