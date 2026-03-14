07ab5122b29d46932d81c74b33e8ba0ec796d3d7
JuanK854 <juanc854ypk@gmail.com>
2026-03-13 18:23:14 -0600

    error de autenticacion Oauth
    agrego flow porque redirect-uri-mismath por los problemas al iniciar sesion en Google y limpio el tailwind ya que estaba de referencia para los bordes

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -6,6 +6,7 @@ function CalendarContent() {
     const [calendarUrl, setCalendarUrl] = useState(null)
 
     const login = useGoogleLogin({
+        flow: "implicit",
         scope: "https://www.googleapis.com/auth/calendar.readonly email profile",
         onSuccess: async (response) => {
             const userInfo = await axios.get(
@@ -13,33 +14,25 @@ function CalendarContent() {
                 { headers: { Authorization: `Bearer ${response.access_token}` } }
             )
             const email = userInfo.data.email
-            setCalendarUrl(`https://calendar.google.com/calendar/embed?src=${encodeURIComponent(email)}&ctz=America%2FMexico_City&bgcolor=%23000000&color=%23ffffff&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=1&showCalendars=0`)
+            setCalendarUrl(`https://calendar.google.com/calendar/embed?src=${encodeURIComponent(email)}&ctz=America%2FMexico_City&bgcolor=%23000000&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=1&showCalendars=0`)
         },
         onError: () => console.error("Error al iniciar sesión")
     })
 
     if (!calendarUrl) {
-    return (
-        <div className="flex items-baseline justify-center h-screen">
-            <div className="flex flex-col items-center gap-6">
-                <span className="text-white text-2xl font-bold py-40 text-center">Utiliza tu calendario en nuestra pagina</span>
-                <button
-                    onClick={() => login()}
-                    className="bg-white text-black px-8 py-3 rounded-xl font-semibold hover:bg-gray-100"
-                >
+        return (
+            <div className="flex flex-col items-center justify-center h-screen gap-6">
+                <span className="text-white text-2xl font-bold">Utiliza tu calendario en nuestra pagina</span>
+                <button onClick={() => login()} className="bg-white text-black px-8 py-3 rounded-xl font-semibold">
                     Conecta tu cuenta de Google, pofis {"<3"}
                 </button>
             </div>
-        </div>
-    )
-}
+        )
+    }
 
     return (
-        <div className="flex items-center justify-center w-full h-screen p-4">
-            <iframe
-                src={calendarUrl}
-                className="w-full h-full rounded-2xl border border-amber-500"
-            />
+        <div className="w-full h-screen p-4">
+            <iframe src={calendarUrl} className="w-full h-full rounded-2xl" />
         </div>
     )
 }
